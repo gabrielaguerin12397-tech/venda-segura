017e07da097aae3fd6a4c3f3e015c0a8acb77e7d
gabrielaguerin12397-tech <gabrielaguerin12397@gmail.com>
2026-05-19 20:15:53 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+window.VENDA_SEGURA_CONFIG = {
+  checkoutUrl: "",
+  supabaseUrl: "https://hmihbeksgibsdoxdvzor.supabase.co",
+  supabaseAnonKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtaWhiZWtzZ2lic2RveGR2em9yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkyMjM4ODYsImV4cCI6MjA5NDc5OTg4Nn0.U2dxamw7ppUTwCzYPe4g8IPMtRKLW36DKrA1j7UlB2w",
+  allowLocalMode: false,
+};
